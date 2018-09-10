775913c340f61d184de517288862ecd386715848
Parag <paragpatel91@gmail.com>
2018-09-10 14:27:03 -0500

    update completed

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.use ((req, res, next) => {
     }
   });
   console.log (log);
-  res.render ('maintanance.hbs', {
-    pageTitle: 'I am developer',
-  });
+  next ();
 });
 app.use (express.static (__dirname + '/public'));
 app.get ('/about', (req, res) => {
